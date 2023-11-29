4cc29d9d1566d2d71ab2401d8bba42b7dbe66c84
Rafael Ceniceros <rrafa7114@gmail.com>
2023-11-29 11:33:06 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import { Footer } from './pages/footer/Footer';
+import Home from './pages/home/Home';
+import { NavBar } from './pages/navbar/NavBar';
+import { Products } from './pages/products/Products';
 
 function App() {
   return (
     <div className="App">
-     
+        <NavBar/>
+        <Home/>
+        <Products/>
+        <Footer/>
     </div>
   );
 }
