cbf033ad0a842bd20d4ade8b77e3c58f790e1863
Rafael Ceniceros <rrafa7114@gmail.com>
2023-11-30 10:22:05 -0600

    changes basic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import { AboutUs } from './pages/about-us/AboutUs';
 import { Footer } from './pages/footer/Footer';
 import Home from './pages/home/Home';
 import { NavBar } from './pages/navbar/NavBar';
@@ -11,6 +12,7 @@ function App() {
         <NavBar/>
         <Home/>
         <Products/>
+        <AboutUs/>
         <Footer/>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 
 export const Button = ({ children, onClick: handleFunction }) => {
     return (
-      <>
+      <> {/* Empty fragment -> evita uso de divs */}
         <button onClick={handleFunction}>{children}</button>
       </>
     );
